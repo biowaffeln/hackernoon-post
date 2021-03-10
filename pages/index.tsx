5b7034ed0800571d2cd26d4e5e41005a2031ff20
Mark Kvetny <m.kvetny@outlook.com>
2021-03-10 17:19:04 +0100

    basic responsive styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import { GetStaticProps } from "next";
 import { FC } from "react";
 import { readFile } from "fs/promises";
 import { Post } from "../types/post";
+import renderHTML from "react-render-html";
+import styles from "./index.module.css";
 
 const Navbar = () => (
   <header className="bg-hn-primary">
@@ -15,17 +17,30 @@ type HomeProps = {
   data: Post;
 };
 
+const parseDate = (publishedAt: number) =>
+  new Date(publishedAt * 1000).toUTCString();
+
 const Home: FC<HomeProps> = ({ data }) => {
   return (
-    <div>
+    <>
       <Navbar />
-      <div>
-        <h1>{data.title}</h1>
-        <p>{data.publishedAt}</p>
-        <img src={data.mainImage} />
-        <div dangerouslySetInnerHTML={{ __html: data.markup }} />
-      </div>
-    </div>
+      <article className={styles.articleContainer}>
+        <div className={styles.fullBleed}>
+          <h1
+            className="font-mono font-bold text-3xl text-center my-6
+                       md:text-4xl lg:text-5xl"
+          >
+            {data.title}
+          </h1>
+          <p>{parseDate(data.publishedAt)}</p>
+          <img
+            className="hover transform duration-200 hover:scale-105 mt-6"
+            src={data.mainImage}
+          />
+        </div>
+        {renderHTML(data.markup)}
+      </article>
+    </>
   );
 };
 
